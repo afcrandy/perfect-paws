6dc3c442dc87d33b433d1e646549d68596c8408a
Andrew Reid <andrew.o.reid@gmail.com>
2025-02-02 11:03:01 +0000

    Wire up password reset link - needs fix to Gmail

--- perfect_paws/settings.py
+++ perfect_paws/settings.py
@@ -159,6 +159,15 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 
+# email backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 # turn off email verification for now
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
